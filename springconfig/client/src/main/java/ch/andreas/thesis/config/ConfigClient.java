7bac692014c645aba8b19cb0912c6597f707ae15
Andreas Heubeck <heuby@gmx.ch>
2017-01-13 14:32:08 +0100

    Added Spring Config Server and Client.

---
+++ springconfig/client/src/main/java/ch/andreas/thesis/config/ConfigClient.java
@@ -0,0 +1,34 @@
+package ch.andreas.thesis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by heuby on 13.01.17.
+ */
+
+
+@SpringBootApplication
+public class ConfigClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class, args);
+    }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message:Hello default}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
